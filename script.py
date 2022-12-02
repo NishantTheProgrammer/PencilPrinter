15b3565125218c82a24d6b6667d47e31264c00e3
NishantTheProgrammer <knishant113@gmail.com>
2022-12-02 23:45:16 +0530

    resized image and showing full numpy array on console

--- script.py
+++ script.py
@@ -1,10 +1,34 @@
 import cv2
 
-image = cv2.imread('nishant.png')
-height, width, channels = image.shape
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-(thresh, bw_image) = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY)
+import sys
+import numpy
+numpy.set_printoptions(threshold=sys.maxsize)
 
-cv2.imwrite('gray.png', gray_image)
-cv2.imwrite('bw_image.png', bw_image)
-print({'height': height, 'width': width})
+def main():
+    image = cv2.imread('nishant.png')
+    height, width, channels = image.shape
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    (thresh, bw_image) = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY)
+    resized = image_resize(bw_image, width=15)
+    cv2.imwrite('gray.png', gray_image)
+    cv2.imwrite('bw_image.png', bw_image)
+    cv2.imwrite('resized.png', resized)
+    print(resized)
+
+
+def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
+    dim = None
+    (h, w) = image.shape[:2]
+    if width is None and height is None:
+        return image
+    if width is None:
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    else:
+        r = width / float(w)
+        dim = (width, int(h * r))
+    resized = cv2.resize(image, dim, interpolation = inter)
+    return resized
+
+main()
