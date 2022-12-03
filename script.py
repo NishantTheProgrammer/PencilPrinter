d16ccb03676b16d0fa0f564139771064ae910cec
NishantTheProgrammer <knishant113@gmail.com>
2022-12-03 21:51:22 +0530

    added print_on_console function

--- script.py
+++ script.py
@@ -9,11 +9,11 @@ def main():
     height, width, channels = image.shape
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     (thresh, bw_image) = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY)
-    resized = image_resize(bw_image, width=15)
+    resized = image_resize(bw_image, width=80)
     cv2.imwrite('gray.png', gray_image)
     cv2.imwrite('bw_image.png', bw_image)
     cv2.imwrite('resized.png', resized)
-    print(resized)
+    print_on_console(resized)
 
 
 def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
@@ -30,5 +30,13 @@ def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
         dim = (width, int(h * r))
     resized = cv2.resize(image, dim, interpolation = inter)
     return resized
+
+
+
+def print_on_console(list_2d):
+    for row in list_2d:
+        for pixel in row:
+            print('*' if pixel > 128 else ' ', end=' ')
+        print()
 
 main()
