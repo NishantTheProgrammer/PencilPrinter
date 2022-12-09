7ca4daff5abd6f95868f7a72c21fd74dabc7974e
nishanttheprogrammer <knishant113@gmail.com>
2022-12-09 23:49:14 +0530

    connected to pyfirmta and blinking LED when star prints

--- script.py
+++ script.py
@@ -2,6 +2,14 @@ import cv2
 
 import sys
 import numpy
+import time
+from pyfirmata import Arduino, util
+
+
+board = Arduino('COM3')
+
+
+
 numpy.set_printoptions(threshold=sys.maxsize)
 
 def main():
@@ -9,7 +17,7 @@ def main():
     height, width, channels = image.shape
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     (thresh, bw_image) = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY)
-    resized = image_resize(bw_image, width=80)
+    resized = image_resize(bw_image, width=10)
     cv2.imwrite('gray.png', gray_image)
     cv2.imwrite('bw_image.png', bw_image)
     cv2.imwrite('resized.png', resized)
@@ -36,7 +44,17 @@ def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
 def print_on_console(list_2d):
     for row in list_2d:
         for pixel in row:
-            print('*' if pixel > 128 else ' ', end=' ')
+            if(pixel > 128):
+                board.digital[13].write(1)
+                time.sleep(0.1)
+                board.digital[13].write(0)
+                print('*', end=' ')
+            else:
+                print(' ', end=' ')
+            sys.stdout.flush()
+            time.sleep(0.2)
         print()
 
 main()
+
+board.digital[13].write(0)
