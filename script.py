dfb37a5fd61d8d276d570a421e612559278bfd84
nishanttheprogrammer <knishant113@gmail.com>
2023-01-01 15:30:00 +0530

    added file picker

--- script.py
+++ script.py
@@ -1,37 +1,30 @@
 import cv2
-
+import serial
 import sys
 import numpy
-import time
-from pyfirmata import Arduino, util, SERVO
-
-servo_pin = 10;
-
-
-board = Arduino('COM3')
-board.digital[servo_pin].mode = SERVO
-
-
-def pull_down():
-    degree = 70
-    board.digital[servo_pin].write(degree)
-    time.sleep(degree * 0.002)
-    board.digital[servo_pin].write(0)
-    time.sleep(degree * 0.002)
-
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog as fd
+from tkinter.messagebox import showinfo
 
 numpy.set_printoptions(threshold=sys.maxsize)
 
+# ser = serial.Serial('COM3', 9600, timeout=1)
+
 def main():
-    image = cv2.imread('nishant.png')
+
+    
+    # ser.write(b'hello')
+    image = cv2.imread('hny.jpg')
     height, width, channels = image.shape
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     (thresh, bw_image) = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY)
-    resized = image_resize(bw_image, width=30)
+    resized = image_resize(bw_image, width=25)
     cv2.imwrite('gray.png', gray_image)
     cv2.imwrite('bw_image.png', bw_image)
     cv2.imwrite('resized.png', resized)
-    print_on_console(resized)
+    print_on_console(resized);
+    # print(repr(resized))
 
 
 def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
@@ -52,20 +45,28 @@ def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
 
 
 def print_on_console(list_2d):
+
     for row in list_2d:
+
         for pixel in row:
-            if(pixel > 128):
-                pull_down()
-                board.digital[13].write(1)
-                time.sleep(0.1)
-                board.digital[13].write(0)
-                print('*', end=' ')
-            else:
-                print(' ', end=' ')
-            sys.stdout.flush()
-            time.sleep(0.01)
+            print('*' if pixel < 128 else ' ', end=' ')
         print()
 
-main()
+# main()
 
-board.digital[13].write(0)
+root = tk.Tk()
+
+
+def select_file():
+    filetypes = (
+        ('text files', '*.txt'),
+        ('All files', '*.*')
+    )
+
+    filename = fd.askopenfilename()
+
+    return filename;
+
+
+
+print(select_file())
